9a75e55c8d3aac1242741d34e084b1d104a7232b
Ozan Uslan <github@ozanuslan.com>
2023-12-30 20:37:24 +0300

    feat(http): handle user-agent route

--- app/http/http.go
+++ app/http/http.go
@@ -7,115 +7,6 @@ import (
 	"strings"
 )
 
-type Request struct {
-	Method   string
-	Path     string
-	Protocol string
-	Headers  []Header
-}
-
-func (r *Request) Unmarshal(buf []byte) {
-	s := string(buf)
-
-	s = strings.Trim(s, "\x00")
-	split := strings.Split(s, "\r\n")
-	fmt.Println(split)
-	for i, line := range split {
-		if i == 0 {
-			r.parseRequestLine(line)
-		} else {
-			r.parseHeaderLine(line)
-		}
-	}
-}
-
-func (r *Request) parseRequestLine(line string) {
-	split := strings.Split(line, " ")
-	r.Method = split[0]
-	r.Path = split[1]
-	r.Protocol = split[2]
-}
-
-func (r *Request) parseHeaderLine(line string) {
-	line = strings.Trim(line, "\r\n")
-	split := strings.Split(line, ": ")
-	if len(split) != 2 {
-		return
-	}
-	r.Headers = append(r.Headers, Header{split[0], split[1]})
-}
-
-type Header struct {
-	Key   string
-	Value string
-}
-
-func NewHeader(key, value string) Header {
-	return Header{key, value}
-}
-
-func (h Header) String() string {
-	return fmt.Sprintf("%s: %s", h.Key, h.Value)
-}
-
-func (h Header) Marshal() []byte {
-	return []byte(h.String())
-}
-
-type Response struct {
-	Protocol string
-	Status   Status
-	Headers  []Header
-	Body     string
-}
-
-func NewResponse() Response {
-	return Response{
-		Protocol: "HTTP/1.1",
-		Status:   OK,
-		Headers:  []Header{},
-		Body:     "",
-	}
-}
-
-func (r *Response) AddHeader(header Header) {
-	r.Headers = append(r.Headers, header)
-}
-
-func (r Response) HeadersString() string {
-	var s string
-	for _, header := range r.Headers {
-		s += header.String() + "\r\n"
-	}
-	return s
-}
-
-func (r Response) Marshal() []byte {
-	return []byte(r.String())
-}
-
-func (r Response) String() string {
-	return fmt.Sprintf("%s %d %s\r\n%s\r\n%s", r.Protocol, r.Status, StatusText(r.Status), r.HeadersString(), r.Body)
-}
-
-type Status int
-
-const (
-	OK       Status = 200
-	NotFound Status = 404
-)
-
-func StatusText(status Status) string {
-	switch status {
-	case OK:
-		return "OK"
-	case NotFound:
-		return "Not Found"
-	}
-
-	return ""
-}
-
 func Serve(addr string) {
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -132,6 +23,32 @@ func Serve(addr string) {
 
 		handle(conn)
 	}
+}
+
+func handleRequest(request Request) Response {
+	response := NewResponse()
+	response.Protocol = request.Protocol
+	response.Status = OK
+	response.AddHeader("Content-Type", "text/plain")
+
+	if request.Path == "/" {
+		return response
+	}
+
+	echoSplit := strings.Split(request.Path, "/echo/")
+	if len(echoSplit) > 1 {
+		response.SetBody(echoSplit[1])
+		return response
+	}
+
+	isUserAgentQuery := strings.HasPrefix(request.Path, "/user-agent")
+	if isUserAgentQuery {
+		response.SetBody(request.Headers["User-Agent"])
+		return response
+	}
+
+	response.Status = NotFound
+	return response
 }
 
 func handle(conn net.Conn) {
@@ -151,29 +68,10 @@ func readRequest(conn net.Conn) Request {
 		os.Exit(1)
 	}
 
-	request := Request{}
+	request := NewRequest()
 	request.Unmarshal(buf)
 
 	return request
-}
-
-func handleRequest(request Request) Response {
-	response := Response{}
-	response.Protocol = request.Protocol
-	response.Status = OK
-	response.AddHeader(NewHeader("Content-Type", "text/plain"))
-
-	if request.Path != "/" {
-		echoSplit := strings.Split(request.Path, "/echo/")
-		if len(echoSplit) > 1 {
-			response.Body = echoSplit[1]
-			response.AddHeader(NewHeader("Content-Length", fmt.Sprintf("%d", len(response.Body))))
-		} else {
-			response.Status = NotFound
-		}
-	}
-
-	return response
 }
 
 func writeResponse(conn net.Conn, response Response) {
@@ -182,4 +80,112 @@ func writeResponse(conn net.Conn, response Response) {
 		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+type Request struct {
+	Method   string
+	Path     string
+	Protocol string
+	Headers  map[string]string
+}
+
+func NewRequest() Request {
+	return Request{
+		Headers: make(map[string]string),
+	}
+}
+
+func (r *Request) Unmarshal(buf []byte) {
+	s := string(buf)
+
+	split := strings.Split(s, "\r\n")
+	fmt.Println(split)
+	for i, line := range split {
+		if i == 0 {
+			r.parseRequestLine(line)
+		} else {
+			r.parseHeaderLine(line)
+		}
+	}
+}
+
+func (r *Request) parseRequestLine(line string) {
+	split := strings.Split(line, " ")
+	r.Method = split[0]
+	r.Path = split[1]
+	r.Protocol = split[2]
+}
+
+func (r *Request) parseHeaderLine(line string) {
+	split := strings.Split(line, ": ")
+	if len(split) < 2 {
+		return
+	}
+	if split[0] == "" {
+		return
+	}
+	r.Headers[split[0]] = split[1]
+}
+
+type Response struct {
+	Protocol string
+	Status   Status
+	Headers  map[string]string
+	Body     string
+}
+
+func NewResponse() Response {
+	return Response{
+		Protocol: "HTTP/1.1",
+		Status:   OK,
+		Headers:  make(map[string]string),
+		Body:     "",
+	}
+}
+
+func (r *Response) AddHeader(key string, value string) {
+	r.Headers[key] = value
+}
+
+func (r Response) HeadersString() string {
+	var s string
+	for key, value := range r.Headers {
+		s += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+	return s
+}
+
+func (r *Response) SetBody(body string) {
+	r.Body = body
+	r.AddHeader("Content-Length", fmt.Sprintf("%d", len(r.Body)))
+}
+
+func (r Response) Marshal() []byte {
+	return []byte(r.String())
+}
+
+func (r Response) String() string {
+	return fmt.Sprintf("%s %d %s\r\n%s\r\n%s",
+		r.Protocol,
+		r.Status,
+		StatusText(r.Status),
+		r.HeadersString(),
+		r.Body)
+}
+
+type Status int
+
+const (
+	OK       Status = 200
+	NotFound Status = 404
+)
+
+func StatusText(status Status) string {
+	switch status {
+	case OK:
+		return "OK"
+	case NotFound:
+		return "Not Found"
+	}
+	return ""
 }
